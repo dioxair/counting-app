1432d96714f9e844f0dac910f6e7e5f4d58beb20
Samuel Olagunju <dioxair@gmail.com>
2022-06-05 18:18:50 -0400

    Added a save count button

--- app.js
+++ app.js
@@ -7,9 +7,23 @@ Reflect that change on the page by changing the number in the HTML document via 
 
 */
 
+/* For saving:
+
+Create a variable called "blob" that will contain the counter number and will be plain text
+Use the FileSaver libraries "saveAs" function and pass in the blob variable, and the name of the plain text file as the arguments
+Put it inside a function called "saveCount()"
+
+*/
+
 let counter = 0
 
 function increaseButton() {
   counter = counter + 1
   document.getElementById("counter").innerText = counter
+}
+
+function saveCount() {
+  var blob = new Blob([`Number: ${counter}`],
+                { type: "text/plain;charset=utf-8" });
+  saveAs(blob, "count.txt")
 }
