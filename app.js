3c1d8ce41f6b8a2b39df64d26097330793459ab4
Samuel Olagunju <dioxair@gmail.com>
2022-06-05 16:47:41 -0400

    Pseudocode for the increase button

---
+++ app.js
@@ -0,0 +1,8 @@
+/* For increasing
+
+Create the counter variable and init as 0
+Listen for interaction (clicks) on the increase button
+Increase the counter variable by 1 each time it's clicked
+Reflect that change on the page by changing the number in the HTML document via ID
+
+*/
