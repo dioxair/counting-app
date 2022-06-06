095c0a9e3b27290e1c32af39258669bf0e6d9a48
Samuel Olagunju <dioxair@gmail.com>
2022-06-05 22:02:30 -0400

    Added functionality to decrease button

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ Listen for interaction (clicks) on the increase button
 Increase the counter variable by 1 each time it's clicked
 Reflect that change on the page by changing the number in the HTML document via ID
 
-*/
+For decreasing:
+Listen for interaction (clicks) on the decrease button
+Decrease the counter variable by 1 each time it's clicked
+Reflect that change on the page by changing the number in the HTML document via getElementById
 
-/* For saving:
+For saving:
 
 Create a variable called "blob" that will contain the counter number and will be plain text
 Use the FileSaver libraries "saveAs" function and pass in the blob variable, and the name of the plain text file as the arguments
@@ -19,6 +22,13 @@ let counter = 0
 
 function increaseButton() {
   counter = counter + 1
+  document.getElementById("counter").innerText = counter
+}
+
+function decreaseButton() {
+  if (counter > 0) {
+    counter = counter - 1
+  }
   document.getElementById("counter").innerText = counter
 }
 
